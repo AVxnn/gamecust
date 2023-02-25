182c69dcb4ce2a924df466fbcfd6820b2fbff9ee
Romashko Georgiy <george@thevann.ru>
2023-02-26 00:21:06 +0300

    fixed

--- components/legendary/common/ImgPopup/index.tsx
+++ components/legendary/common/ImgPopup/index.tsx
@@ -1,20 +1,65 @@
-import React from 'react';
-import {useDispatch} from "react-redux";
+import React, {useRef, useState} from 'react';
 import styles from './ImgPopup.module.scss'
-import {addImg, open} from "../../../../features/Popup/PopupSlice";
 import Image from 'next/image'
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const ImgPopup = ({data} : any) => {
 
-  const dispatch = useDispatch()
-  const openPopup = () => {
-    dispatch(open())
-    dispatch(addImg(data.img))
+  const [openPopup, setOpenPopup] = useState(false)
+  const [dataImg, setDataImg] = useState({
+    width: 100,
+    height: 100,
+    left: 0,
+    top: 0,
+  })
+
+  const imgRef = useRef<HTMLImageElement>(null);
+  const img= useRef<HTMLImageElement>(null);
+
+
+  const openFunc = () => {
+    if(!img?.current) return
+    setDataImg(img?.current?.getBoundingClientRect())
+    setOpenPopup(true)
+    console.log(dataImg)
+
   }
 
   return (
     <>
-      <Image className={styles.imgPopup} onClick={() => openPopup()} width={600} height={500} src={data.img} alt=""/>
+      <div className={`${styles.container}`}>
+          <Image
+            className={styles.imgPopup}
+            ref={img}
+            onClick={() => openFunc()}
+            width={600}
+            height={500}
+            src={data.img}
+            alt=""/>
+      </div>
+      {
+        openPopup && (
+          <div
+            onClick={() => setOpenPopup(false)}
+            className={styles.fixed}>
+            <TransformWrapper>
+              <TransformComponent>
+                <Image
+                  className={styles.imgPopup}
+                  ref={imgRef}
+                  style={{ "--left": dataImg ? `${dataImg?.left}px` : 'auto',
+                    "--top": dataImg ? `${dataImg?.top}px` : 'auto',
+                    "--width": dataImg ? `${dataImg?.width}px` : 'auto',
+                    "--height": dataImg ? `${dataImg?.height}px` : 'auto'} as React.CSSProperties}
+                  width={dataImg?.width}
+                  height={dataImg?.height}
+                  src={data.img}
+                  alt=""/>
+              </TransformComponent>
+            </TransformWrapper>
+          </div>
+        )
+      }
     </>
   );
 };
