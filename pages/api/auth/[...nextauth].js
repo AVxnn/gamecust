f27a3efe5bfaed82c144249c844bc45d514be275
Georgiy Romashko <george@thevann.ru>
2023-11-21 21:15:25 +0300

    f

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,58 +1,27 @@
 import NextAuth from "next-auth";
 import VkProvider from "next-auth/providers/vk";
 import DiscordProvider from "next-auth/providers/discord";
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 const scopes = ['identify', 'email'].join(' ')
 
 export const nextAuthOptions = {
-  debug: true,
   providers: [
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID ?? "",
-      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? "",
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
       authorization: { params: { scope: scopes } },
-      allowDangerousEmailAccountLinking: true,
     }),
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      allowDangerousEmailAccountLinking: true,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRE,
     }),
     VkProvider({
-      clientId: process.env.VK_CLIENT_ID ?? "",
-      clientSecret: process.env.VK_CLIENT_SECRET ?? "",
-      allowDangerousEmailAccountLinking: true,
+      clientId: process.env.VK_CLIENT_ID,
+      clientSecret: process.env.VK_CLIENT_SECRET,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    session: async (session, user) => {
-      session.user = user;
-      return Promise.resolve(session);
-    },
-    async signIn({ user, account, profile }) {
-      console.log("signIn", user, account, profile);
-      
-      if(!user.email) return false;
-
-      // Check if the user is allowed to sign in
-      const invite = await getInvite(user.email);
-      // Check if invite exists
-      if (!invite) return false;
-
-      // Check if invite is allowed to sign in
-      const isAllowedToSignIn = invite.invited;
-      if (isAllowedToSignIn) {
-        return true;
-      } else {
-        // Return false to display a default error message
-        return false;
-        // Or you can return a URL to redirect to:
-        // return '/unauthorized'
-      }
-    },
-  }
 }
 
 export default NextAuth(nextAuthOptions);
